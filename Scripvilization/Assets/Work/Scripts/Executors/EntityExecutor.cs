c0202ebb153f10ae65fe7f31052cd2b1d5561f8c
machany <machanicaly@gmail.com>
2025-06-01 15:58:15 +0900

    feat : executor

---
+++ Scripvilization/Assets/Work/Scripts/Executors/EntityExecutor.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Work.Scripts.Core.Finders;
+using Assets.Work.Scripts.Core.Managers;
+using Assets.Work.Scripts.Sriptable;
+using MethodArchiveSystem;
+using UnityEngine;
+
+namespace Assets.Work.Scripts.Executors
+{
+    public class EntityExecutor : MonoBehaviour, IExecutor
+    {
+        [SerializeField] private ObjectFinder gridManagerFinder;
+
+        private static GridManager _gridManager;
+
+        private void Awake()
+        {
+            _gridManager = gridManagerFinder.GetObject<GridManager>();
+        }
+
+        [ArchiveMethod("이동")]
+        public static void Move(IScriptable target, string[] direction)
+        {
+            _gridManager.Grid.GetWorldToNodePoint(target.Object.transform.position);
+            target.Object.transform.position = Vector3.zero;
+        }
+    }
+}
---
+++ Scripvilization/Assets/Work/Scripts/Executors/IExecutor.cs
@@ -0,0 +1,9 @@
+﻿using MethodArchiveSystem;
+
+namespace Assets.Work.Scripts.Executors
+{
+    public interface IExecutor : IArchivedMethods // 모든 아카이브 메서드들은 이 인터페이스를 상속받아 구현하게끔
+    {
+        // GameObject, params string[] parameters;
+    }
+}
