ee1f43cbe9eb88bfb703c340c6a07202c080b4bb
machany <machanicaly@gmail.com>
2025-06-01 15:57:57 +0900

    feat : scriptable

---
+++ Scripvilization/Assets/Work/Scripts/Scriptables/IScriptable.cs
@@ -0,0 +1,21 @@
+﻿using AgamaLibrary.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Work.Scripts.Sriptable
+{
+    public interface IScriptable
+    {
+        public GameObject Object { get; }
+        public string ObjectName { get; }
+
+        public NotifyValue<bool> CanExecuteCommandState { get; }
+
+        public void Execute();
+        public void Complete();
+    }
+}
---
+++ Scripvilization/Assets/Work/Scripts/Scriptables/ScriptableEntity.cs
@@ -0,0 +1,45 @@
+﻿using AgamaLibrary.DataStructures;
+using AgamaLibrary.Unity.EventSystem;
+using Assets.Work.Scripts.Core.Events;
+using Assets.Work.Scripts.Entities;
+using Assets.Work.Scripts.Sriptable;
+using UnityEngine;
+
+namespace Assets.Work.Scripts.Scriptables
+{
+    public class ScriptableEntity : Entity, IScriptable
+    {
+        [SerializeField] protected EventChannelSO objectMangeEventChannel;
+
+        public GameObject Object => gameObject;
+        [field : SerializeField] // test
+        public string ObjectName {  get; protected set; }
+
+        public NotifyValue<bool> CanExecuteCommandState { get; protected set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            CanExecuteCommandState = new NotifyValue<bool>();
+        }
+
+        protected virtual void OnEnable()
+        {
+            objectMangeEventChannel.InvokeEvent(ObjectManageEvents.ChangeObjectEvent.Initialize(true, ObjectName, this));
+        }
+
+        protected virtual void OnDisable()
+        {
+            objectMangeEventChannel.InvokeEvent(ObjectManageEvents.ChangeObjectEvent.Initialize(false, ObjectName, this));
+        }
+
+        public void Execute()
+        {
+        }
+
+        public void Complete()
+        {
+        }
+    }
+}
